51ecd52726a836a852bc1f1f1e08c696728efb56
giraffester <legogiraffester@gmail.com>
2024-12-05 23:59:46 -0800

    Fixed Enemy and Object Health

---
+++ Assets/EnemyHealth.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    [SerializeField] private float startingDamage;
+    public float invulTime = 1f; // The time you stay invulnerable after a hit
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool invulnerable = false;
+    private bool dead;
+
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        TakeDamage(startingDamage);
+        anim = GetComponent<Animator>();
+
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        if (!invulnerable)
+        {
+            currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+            StartCoroutine(JustHurt());
+        }
+
+        if (currentHealth > 0)
+        {
+            if (anim != null)
+            {
+                anim.SetBool("hurt", true);//player hurt
+            }
+        }
+        else
+        {
+            if (currentHealth == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+
+    IEnumerator JustHurt()
+    {
+        invulnerable = true;
+        yield return new WaitForSeconds(invulTime);
+        invulnerable = false;
+
+    }
+}
+
